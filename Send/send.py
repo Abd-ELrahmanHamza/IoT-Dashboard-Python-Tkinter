0fdfbde4d9877f0338f2502baaf1b06301e1beb9
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-09-29 13:45:52 +0200

    modify send

--- Send/send.py
+++ Send/send.py
@@ -2,59 +2,92 @@ from tkinter.constants import END
 from typing import Text
 import paho.mqtt.publish as publish
 import tkinter as tk
+import tkinter.font as font
 from tkinter.ttk import *
+
+
 def Send():
-    Server=en1.get()
-    Topic=en2.get()
-    message=en3.get()
-    publish.single(Topic,message,hostname=Server)
+    Server = en1.get()
+    Topic = en2.get()
+    message = en3.get()
+    publish.single(Topic, message, hostname=Server)
+
+
 def close():
     window.destroy()
+
+
 def clear():
-    en1.delete(0,"end")
-    en2.delete(0,"end")
-    en3.delete(0,"end")
+    en1.delete(0, "end")
+    en2.delete(0, "end")
+    en3.delete(0, "end")
+
+
 def Default():
     clear()
-    en1.insert(END,'192.168.1.2')
-    en2.insert(END,"IoT/H1")
-    en3.insert(END,"Default messages")
+    en1.insert(END, 'broker.mqttdashboard.com')
+    en2.insert(END, "Topic")
+    en3.insert(END, "Default messages")
+
 
 window = tk.Tk()
 window.title("Publisher")
-window.geometry("600x600")
+window.geometry("500x300")
 
-img =tk.PhotoImage(file="PNG_transparency_demonstration_1.png")
-img1 = img.subsample(5,5)
-lbl4 = tk.Label(window,image=img1)
-lbl4.grid(row=2,column=5,columnspan=2,rowspan=1)
+# img = tk.PhotoImage(file="PNG_transparency_demonstration_1.png")
+# img1 = img.subsample(5, 5)
+# lbl4 = tk.Label(window, image=img1)
+# lbl4.grid(row=2, column=5, columnspan=2, rowspan=1)
+title = tk.Label(window, text="Send", fg="red",
+                 font=("Helvetica", 20, "bold"), width=30)
+title.grid(row=0, column=0, columnspan=20, rowspan=2)
 
-lbl1 = tk.Label(window,text = "Server:")
-lbl2 = tk.Label(window,text = "Topic:")
-lbl3 = tk.Label(window,text = "Message:")
+labelsWidth = 10
+labelsHeight = 2
 
-en1 = tk.Entry(window,text="Enter your server")
-en2 = tk.Entry(window,text="Enter your Topic")
-en3 = tk.Entry(window,text="Enter your Message")
+labelsFont = font.Font(family='Helvetica', size=10, weight='bold')
+lbl1 = tk.Label(window, text="Server:", 
+                width=labelsWidth, height=labelsHeight)
+lbl2 = tk.Label(window, text="Topic:", 
+                width=labelsWidth, height=labelsHeight)
+lbl3 = tk.Label(window, text="Message:",
+                width=labelsWidth, height=labelsHeight)
+lbl1['font'] = lbl2['font'] = lbl3['font'] = labelsFont
 
-sendbt = tk.Button(window,text = "send",command=Send)
-closebt = tk.Button(window,text = "Close",command=close)
-clearbt = tk.Button(window,text="clear",command=clear)
-defaultbt = tk.Button(window,text="Default",command=Default)
+entityWidth = 30
 
-lbl1.grid(row=2,column=2)
-lbl2.grid(row=3,column=2)
-lbl3.grid(row=4,column=2)
-en1.grid(row=2,column=3)
-en2.grid(row=3,column=3)
-en3.grid(row=4,column=3)
-sendbt.grid(row=5,column=4)
-closebt.grid(row=5,column=5)
-clearbt.grid(row=5,column=3)
-defaultbt.grid(row=5,column=2)
+en1 = tk.Entry(window, text="Enter your server",width = entityWidth)
+en2 = tk.Entry(window, text="Enter your Topic",width = entityWidth)
+en3 = tk.Entry(window, text="Enter your Message", width=entityWidth)
+
+buttonsWidth = 10
+buttonsHeight = 2
+buttonsColSpan = 3
+
+bottonsFont = font.Font(family='Helvetica', size=10, weight='bold')
+sendbt = tk.Button(window, text="send", command=Send,
+                   bg="#33FF98", height=buttonsHeight, width=buttonsWidth)
+closebt = tk.Button(window, text="Close", command=close,
+                    bg="#FF5733", height=buttonsHeight, width=buttonsWidth)
+clearbt = tk.Button(window, text="clear", command=clear,
+                    bg="#33A7FF", height=buttonsHeight, width=buttonsWidth)
+defaultbt = tk.Button(window, text="Default", command=Default,
+                      bg="#FFC733", height=buttonsHeight, width=buttonsWidth)
+sendbt['font'] = closebt['font'] = clearbt['font'] = defaultbt['font'] = bottonsFont
+
+rowSpan = 2
+columnSpan = 3
+lbl1.grid(row=2, column=1,columnspan=columnSpan ,rowspan = rowSpan)
+lbl2.grid(row=rowSpan+3, column=1, columnspan=columnSpan, rowspan=rowSpan)
+lbl3.grid(row=rowSpan+5, column=1, columnspan=columnSpan, rowspan=rowSpan)
+
+en1.grid(row=2, column=2+columnSpan, columnspan=columnSpan+3, rowspan=rowSpan)
+en2.grid(row=rowSpan+3, column=2+columnSpan, columnspan=columnSpan+3, rowspan=rowSpan)
+en3.grid(row=rowSpan+5, column=2+columnSpan, columnspan=columnSpan+3, rowspan=rowSpan)
+
+sendbt.grid(row=rowSpan+8, column=1,columnspan=columnSpan, rowspan=rowSpan+2)
+closebt.grid(row=rowSpan+8, column=5,columnspan=columnSpan, rowspan=rowSpan+2)
+clearbt.grid(row=rowSpan+8, column=9,columnspan=columnSpan, rowspan=rowSpan+2)
+defaultbt.grid(row=rowSpan+8, column=13, columnspan=columnSpan, rowspan=rowSpan+2)
+
 window.mainloop()
-
-Server = "192.168.1.2"
-Topic = "IoT/H1"
-h=60
-#publish.single(Topic,"this is value from H1 =%d"%h,hostname=Server)
